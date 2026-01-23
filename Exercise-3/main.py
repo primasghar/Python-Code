501b8b8ce74c7d985a457799dc316ad3ae344fa2
Prim Asghar <mirpasghar@gmail.com>
2026-01-24 00:29:34 +0200

    Problem 3 solved

--- Exercise-3/main.py
+++ Exercise-3/main.py
@@ -23,25 +23,53 @@
 # C: windowless cabin below the car deck.
 # If the user enters an invalid cabin class, the program outputs an error message Invalid cabin class.
 
-cabin= input("Choose your cabin class: LUX, A, B, or C . ")
+# cabin= input("Choose your cabin class: LUX, A, B, or C . ")
 
-if cabin == "LUX":
-    print("LUX: upper-deck cabin with a balcony.")
-elif cabin == "A":
-    print("A: above the car deck, equipped with a window.")
-elif cabin == "B":
-    print("B: windowless cabin above the car deck.")
-elif cabin == "C":
-    print("windowless cabin below the car deck.")
-else:
-    print("Invalid cabin class.")
-
-
-
+# if cabin == "LUX":
+#     print("LUX: upper-deck cabin with a balcony.")
+# elif cabin == "A":
+#     print("A: above the car deck, equipped with a window.")
+# elif cabin == "B":
+#     print("B: windowless cabin above the car deck.")
+# elif cabin == "C":
+#     print("windowless cabin below the car deck.")
+# else:
+#     print("Invalid cabin class.")
 
 # ---------------------------------------------------------------------------------------------------------------------
-#3. Write a program that asks for the biological gender and hemoglobin value (g/l). The program the notifies the user if the hemoglobin value is low, normal or high.
+#3. Write a program that asks for the biological gender and hemoglobin value (g/l).
+# The program the notifies the user if the hemoglobin value is low, normal or high.
 
 # A normal hemoglobin value for adult females is between 117-155 g/l.
 # A normal hemoglobin value for adult males is between 134-167 g/l.
-# Write a program that asks the user to enter a year and notifies the user whether the input year is a leap year. A year is a leap year if it is divisible by four. However, years divisible by 100 are leap years only if they are also divisible by 400.
+
+gender = input("Choose your biological gender: Female or Male: ")
+
+if gender == "Female":
+    hemoglobin_value = int(input("Enter your hemoglobin value (g/l): "))
+    if hemoglobin_value > 155:
+        print("You have high Hb.")
+    elif hemoglobin_value < 117:
+        print("You have low Hb.")
+    else:
+        print("You have normal Hb.")
+elif gender == "Male":
+     hemoglobin_value = int(input("Enter your hemoglobin value (g/l): "))
+     if hemoglobin_value > 167:
+         print("You have high Hb.")
+     elif hemoglobin_value < 134:
+         print("You have low Hb.")
+     else:
+         print("You have normal Hb.")
+else:
+    print("Invalid input.")
+
+
+
+
+
+
+
+
+
+#4. Write a program that asks the user to enter a year and notifies the user whether the input year is a leap year. A year is a leap year if it is divisible by four. However, years divisible by 100 are leap years only if they are also divisible by 400.
