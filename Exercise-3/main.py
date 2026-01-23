15902ee7d04a61a3ae19f76eb90ec573c8f2bd85
Prim Asghar <mirpasghar@gmail.com>
2026-01-23 23:50:35 +0200

    Problem 1 solved

---
+++ Exercise-3/main.py
@@ -0,0 +1,34 @@
+#1. Write a program that asks a fisher the length of a zander in centimeters.
+# If the zander does not fulfill the size limit, the program instructs to release the fish back into the lake
+# and notifies the user of how many centimeters below the size limit the caught fish was. A zander must be 42 centimeters
+# or longer to meet the size limit.
+
+optimal_length = 42
+
+zander_length = float(input("Enter length of the zander in cm. "))
+
+if zander_length >= optimal_length:
+    print("Great! The zander fulfills the size limit criteria. ")
+else:
+    size_eval = round(optimal_length - zander_length)
+    print(f"Please release the fish back into the lake. It is {size_eval} cm smaller than optimal size required for harvesting. ")
+
+# ---------------------------------------------------------------------------------------------------------------------
+#2. Write a program that asks the user to enter the cabin class of a cruise ship and then prints out a written
+# description according to the list below. You must use an if/elif/else structure in your solution.
+
+# LUX: upper-deck cabin with a balcony.
+# A: above the car deck, equipped with a window.
+# B: windowless cabin above the car deck.
+# C: windowless cabin below the car deck.
+# If the user enters an invalid cabin class, the program outputs an error message Invalid cabin class.
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#3. Write a program that asks for the biological gender and hemoglobin value (g/l). The program the notifies the user if the hemoglobin value is low, normal or high.
+
+# A normal hemoglobin value for adult females is between 117-155 g/l.
+# A normal hemoglobin value for adult males is between 134-167 g/l.
+# Write a program that asks the user to enter a year and notifies the user whether the input year is a leap year. A year is a leap year if it is divisible by four. However, years divisible by 100 are leap years only if they are also divisible by 400.
