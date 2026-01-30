55d15356015c78cd4d9e264906c3521d537a4ffa
Prim Asghar <mirpasghar@gmail.com>
2026-01-30 22:51:25 +0200

    Module 5: Problem 1 and 2 done

---
+++ Exercise-5/main.py
@@ -0,0 +1,56 @@
+import random
+# 1. Write a program that asks the user how many dice to roll. The program rolls all the dice once and prints out the sum
+# of the numbers. Use a for loop.
+
+# print("Dice roll")
+#
+# number_of_dice = int(input("Enter the number of dice you want to roll: "))
+#
+# dice=0
+# result=0
+#
+# for i in range(number_of_dice):
+#     dice=dice+1
+#     num_output = random.randint(1,6+1)
+#     print(f"{dice}-{num_output}")
+#     result += num_output
+#
+# print(f"The result is {result}")
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 2. Write a program that asks the user to enter numbers until they input an empty string to quit. At the end, the program
+# prints out the five greatest numbers sorted in descending order. Hint: You can reverse the order of sorted list items
+# by using the sort method with the reverse=True argument.
+
+print("Five greatest numbers")
+
+number_list = []
+
+while True:
+    try:
+        number = int(input("Enter the number: "))
+        number_list.append(number)
+    except ValueError:
+        number_list.sort(reverse=True)
+        for i in range(5):
+            print(number_list[i])
+        break
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 3. Write a program that asks the user for an integer and tells if the number is a prime number. Prime numbers are number
+# that are only divisible by one or the number itself.
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# For example, 13 is a prime number as it can only be divided by 1 or 13 so that the result is an integer.
+# On the other hand, 21 is not a prime number as it is divisible by 3 and 7.
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 4. Write a program that asks the user to enter the names of five cities one by on (use a for loop for reading the names)
+# and stores them into a list structure. Finally, the program prints out the names of the cities one by one, one city
+# per line, in the same order they were read as input. Use a for loop for asking the names and a for/in loop to iterate
+# through the list.
