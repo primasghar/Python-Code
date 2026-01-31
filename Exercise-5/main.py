0e9b33f37a071168e33764973e0d241c52e98bae
Prim Asghar <mirpasghar@gmail.com>
2026-01-31 14:20:18 +0200

    Module 5: 4th solved

--- Exercise-5/main.py
+++ Exercise-5/main.py
@@ -1,8 +1,8 @@
-import random
-from symbol import factor
+
 
 # 1. Write a program that asks the user how many dice to roll. The program rolls all the dice once and prints out the sum
 # of the numbers. Use a for loop.
+# import random
 
 # print("Dice roll")
 #
@@ -49,33 +49,58 @@ from symbol import factor
 # 3. Write a program that asks the user for an integer and tells if the number is a prime number. Prime numbers are number
 # that are only divisible by one or the number itself.
 
-print("Find out if the number is a Prime number")
-
-user_number= int(input("Enter the number: "))
-
-list_of_factors = []
-
-for i in range(1, user_number+1):
-    print(i)
-    if user_number % i == 0:
-        list_of_factors.append(i)
-        print(list_of_factors)
-
-if len(list_of_factors) == 2 and list_of_factors[0] == 1 and list_of_factors[1] == user_number:
-    print("The number is a Prime number")
-else:
-    print("The number is not a Prime number")
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-# For example, 13 is a prime number as it can only be divided by 1 or 13 so that the result is an integer.
-# On the other hand, 21 is not a prime number as it is divisible by 3 and 7.
-
-
-
+# print("Find out if the number is a Prime number")
+#
+# user_number= int(input("Enter the number: "))
+#
+# list_of_factors = []
+#
+# for i in range(1, user_number+1):
+#     print(i)
+#     if user_number % i == 0:
+#         list_of_factors.append(i)
+#         print(list_of_factors)
+#
+# if len(list_of_factors) == 2 and list_of_factors[0] == 1 and list_of_factors[1] == user_number:
+#     print("The number is a Prime number")
+# else:
+#     print("The number is not a Prime number")
 
 # ----------------------------------------------------------------------------------------------------------------------
 # 4. Write a program that asks the user to enter the names of five cities one by on (use a for loop for reading the names)
 # and stores them into a list structure. Finally, the program prints out the names of the cities one by one, one city
 # per line, in the same order they were read as input. Use a for loop for asking the names and a for/in loop to iterate
 # through the list.
+
+
+print("Five cities")
+
+city_name_list = []
+
+for x in range(5):
+    user_entered_city = input("Enter a city: ")
+
+    if user_entered_city in city_name_list:
+        print("This city name already exists. Please enter a new city name.")
+        user_entered_city = input("Enter a city: ")
+
+    city_name_list.append(user_entered_city)
+
+for city_name in city_name_list:
+    print(city_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
