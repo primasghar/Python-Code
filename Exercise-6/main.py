2479be710da11b519c32ecd36e89e6ef0b6bd9b0
Prim Asghar <mirpasghar@gmail.com>
2026-01-31 15:18:08 +0200

    Module 6: exercise 1 solved

---
+++ Exercise-6/main.py
@@ -0,0 +1,56 @@
+# 1. Write a function that returns a random dice roll between 1 and 6. The function should not have any parameters. Write
+# a main program that rolls the dice until the result is 6. The main program should print out the result of each roll.
+import random
+
+def roll_dice():
+    return random.randint(1,6)
+
+result = 0
+
+while result != 6:
+    result = roll_dice()
+    print(result)
+
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 2. Modify the function above so that it gets the number of sides on the dice as a parameter. With the modified function
+# you can for example roll a 21-sided role-playing dice. The difference to the last exercise is that the dice rolling
+# in the main program continues until the program gets the maximum number on the dice, which is asked from the user at
+# the beginning.
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 3. Write a function that gets the quantity of gasoline in American gallons and returns the number converted to liters.
+# Write a main program that asks for a volume in gallons from the user and converts the value to liters. The conversion
+# must be done by using the function. Conversions continue until the user inputs a negative value.
+
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 4. Write a function that gets a list of integers as a parameter. The function returns the sum of all the numbers in the
+# list. For testing, write a main program where you create a list, call the function, and print out the value it returned.
+
+
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 5. Write a function that gets a list of integers as a parameter. The function returns a second list that is otherwise the
+# same as the original list except that all uneven numbers have been removed. For testing, write a main program where you
+# create a list, call the function, and then print out both the original as well as the cut-down list.
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 6. Write a function that receives two parameters: the diameter of a round pizza in centimeters and the price of the pizza
+# in euros. The function calculates and returns the unit price of the pizza per square meter. The main program asks the
+# user to enter the diameter and price of two pizzas and tells the user which pizza provides better value for money
+# (which of them has a lower unit price). You must use the function you wrote for calculating the unit prices.
