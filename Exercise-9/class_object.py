73ddfd146195499f285c5969d782eeea741eb622
Prim Asghar <mirpasghar@gmail.com>
2026-03-19 12:18:39 +0200

    Module 9: Part 2 Almost Done.

--- Exercise-9/class_object.py
+++ Exercise-9/class_object.py
@@ -1,9 +1,16 @@
 class Car:
-    def __init__(self, registration_number, maximum_speed, current_speed =0,travelled_distance=0):
+    def __init__(self, registration_number, maximum_speed, current_speed = 0,travelled_distance = 0):
         self.registration_number = registration_number
         self.maximum_speed = maximum_speed
         self. current_speed = current_speed
         self.travelled_distance = travelled_distance
+
+    def accelerate(self, change_speed):
+        self.current_speed = self.current_speed + change_speed
+
+        if self.current_speed < 0 or self.current_speed > self.maximum_speed:
+            pass
+
 
 new_car = Car( "ABC-123", 142)
 
@@ -12,3 +19,10 @@ print(f"""Car details:
         Maximum speed: {new_car.maximum_speed} km/h
         Current speed: {new_car.current_speed} km/h
         Distance travelled: {new_car.travelled_distance}km")""")
+
+new_car.accelerate(30)
+new_car.accelerate(70)
+new_car.accelerate(50)
+print(f"current speed: {new_car.current_speed}")
+new_car.accelerate(-200)
+print(f"current speed: {new_car.current_speed}")
