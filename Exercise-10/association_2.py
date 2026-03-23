e52f85e7224a6a8dc558218b532ca0f501716be4
Prim Asghar <mirpasghar@gmail.com>
2026-03-23 18:18:25 +0200

    Module 10: Part 2: Done

---
+++ Exercise-10/association_2.py
@@ -0,0 +1,62 @@
+
+# 2- Extend the previous program by creating a Building class. The initializer parameters for the class are the numbers
+# of the bottom and top floors and the number of elevators in the building.
+# When a building is created, the building creates the required no of elevators. The list of elevators is stored as a property of the building.
+# Write a method called run_elevator that accepts the number of the elevator and the destination floor as its parameters.
+# In the main program, write the statements for creating a new building and running the elevators of the building.
+
+class Elevator:
+
+    def __init__(self, bottom_floor, top_floor):
+        if bottom_floor >= top_floor:
+            print("Please enter a valid bottom floor number. It should be less than bottom floor")
+        elif top_floor <= bottom_floor:
+            print("Please enter a valid top floor number. It should be greater than bottom floor")
+
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+        self.current_floor = bottom_floor
+
+    def go_to_floor(self, floor_no):
+        print(f"\nCurrently at floor:{self.current_floor}")
+        print(f"Go to: {floor_no}\n")
+
+        if floor_no > self.current_floor:
+            print("Going Up")
+            for x in range(self.current_floor, floor_no):
+                self.floor_up()
+                print(f"Floor {self.current_floor}")
+
+        elif floor_no < self.current_floor:
+            print("Going Down")
+            for x in range(floor_no, self.current_floor):
+                self.floor_down()
+                print(f"Floor {self.current_floor}")
+
+    def floor_up(self):
+        self.current_floor = self.current_floor + 1
+
+    def floor_down(self):
+        self.current_floor = self.current_floor - 1
+
+class Building:
+    def __init__(self, building_bottom_floor, building_top_floor, total_elevators):
+
+        self.all_elevators =  []
+        for x in range(total_elevators):
+            self.all_elevators.append(Elevator(building_bottom_floor, building_top_floor))
+
+        print(f"{len(self.all_elevators)}")
+
+    def run_elevator(self, elevator_no, destination_floor):
+        building_elevators = len(self.all_elevators)
+        if elevator_no <= building_elevators:
+            self.all_elevators[elevator_no].go_to_floor(destination_floor)
+            print(f"Elevator-{elevator_no} is at { self.all_elevators[elevator_no].current_floor} floor")
+        else:
+            print(f"{elevator_no} does not exist")
+
+building_1 = Building(0,6, 2)
+building_1.run_elevator(1, 4)
+
+
